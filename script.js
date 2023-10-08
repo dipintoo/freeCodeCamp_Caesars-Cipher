f1a37c89740bb01f66d1fa735abc1e88d0a9803a
dipintoo <120252968+dipintoo@users.noreply.github.com>
2023-10-08 08:11:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 // STEP //
 
 // 1. Convert semua input huruf menjadi uppercase dan hilangkan semua karakter selain huruf besar
-
 // 2. Karena makna setiap huruf akan bergeser sebanyak 13 huruf, kita perlu mengubah input huruf menjadi angka dahulu dengan memanfaatkan kode ASCII dimana kode ASCII untuk A-Z adalah 65-90 agar bisa melakukan operasi matematika.
-
 // 3. Untuk memperoleh kode yang tepat, berikut langkah yang terpikirkan:
 
 // Misal input adalah "S" dan kita ingin outputnya "F".
-
 // a. Kode ASCII dari "S" adalah 83. Karena range kode ASCII untuk A-Z adalah 65-90, kita perlu mengurangi 83 dengan 65 untuk membawa karakter huruf ke dalam indeks nol berdasarkan alfabet. 83 - 65 = 18
-
 // b. Untuk pergeseran ROT13 kita tambahkan 13 ke 18. 18 + 13 = 31
-
 // c. Alfabet terdiri dari 26 huruf. Jika penjumlahan melebihi 26 dan kita ingin perhitungan kembali lagi ke awal alfabet, kita perlu melakukan operasi modulus. 31 mod 26 menghasilkan sisa 5, bergeser 5 huruf dari awal alfabet.
-
 // d. Supaya masuk ke dalam range kode ASCII yaitu 65-90, kita harus tambahkan 5 dengan 65: 5 + 65 = 70.
 
 // 4. Langkah akhir yaitu mengubah kode ASCII yang di peroleh kembali menjadi huruf yaitu dari 70 --> "F"
